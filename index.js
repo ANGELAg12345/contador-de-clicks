8a55d34f93f8d131e12ab9955a494ffe3dc9c9dc
ANGELAg12345 <112735693+ANGELAg12345@users.noreply.github.com>
2023-04-01 10:56:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from render;
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
